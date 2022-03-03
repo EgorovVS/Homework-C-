e1fc86193c2eb25665d75d73d48b9881313514bf
Spoon2 <viktor.spoon2@gmail.com>
2022-03-03 12:15:28 +0400

    Task2 created

---
+++ task2/Task2.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Программа для нахождения максимума из трех чисел");
+
+Console.WriteLine("Введите первое число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int j = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int max = i;
+if(j>i){
+    max = j;
+}
+if(k>j){
+    max = k;
+}
+Console.WriteLine("Максимальное число - "+ max);
