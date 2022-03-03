2465d4b729b995dda796108b31f1def334489158
Spoon2 <viktor.spoon2@gmail.com>
2022-03-03 12:40:49 +0400

    Task3 created

---
+++ task3/Task3.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Программа для определения четности числа");  
+
+Console.WriteLine("Введите число");
+
+int i = Convert.ToInt32(Console.ReadLine());
+
+if(i%2==0){
+    Console.WriteLine("Число "+ i + "четное");
+} 
+else {
+    Console.WriteLine("Число "+ i + " нечетное");
+}
