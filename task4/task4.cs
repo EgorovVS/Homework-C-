88e28cbb8a297627cb923ef9d7b14655611db533
Spoon2 <viktor.spoon2@gmail.com>
2022-03-03 13:01:09 +0400

    Task4 created

---
+++ task4/task4.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Программа отображения четных чисел. При вводе N выводит все четные числа от 1 до N");
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+for(i = 2; i<N; i+=2){
+Console.WriteLine(i);
+}
