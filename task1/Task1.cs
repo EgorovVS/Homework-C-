5c5ea84a6138e176e122b5d160d32304037ddac8
Spoon2 <viktor.spoon2@gmail.com>
2022-03-03 11:53:26 +0400

    Task1 created

---
+++ task1/Task1.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Вас приветствует программа нахождения большего из двух чисел");
+
+Console.WriteLine("Введите первое число");
+
+int i = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+
+int j = Convert.ToInt16(Console.ReadLine());
+
+if(i==j){
+    Console.WriteLine("Числа равны");
+}
+else if(i>j){
+    Console.WriteLine(i + " больше, чем " + j);
+}
+else {
+    Console.WriteLine(j + " больше, чем " + i);
+}
+   
+        
+    
